9ddababda08013968a7b18d72b3269d8b4850ca8
Rodrigo Labrador <headstylecolorred@gmail.com>
2025-05-27 19:00:00 +0100

    added lidstone smoothing

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use utils::data_parser::DataParser;
 use utils::test_email_reader::read_test_email;
 use std::env;
 use smoothing::laplace::Laplace;
+use smoothing::lidstone::Lidstone;
 mod utils;
 mod smoothing;
 
@@ -26,14 +27,37 @@ fn main() {
     let test_email_tokens = read_test_email(email_file_path);
 
     // Apply the Laplace smoothing to the test email tokens to get the probability of the email being ham or spam
-    let laplace = Laplace::new(&data_parser, 1.0, &test_email_tokens);
+    let laplace_smoothing_result: String = laplace_smoothing_result(&data_parser, &test_email_tokens);
+    let lidstone_smoothing_result: String = lidstone_smoothing_result(&data_parser, &test_email_tokens);
+
+    // Print the results
+    println!("--------------------------------");
+    println!("Laplace smoothing result: {}", laplace_smoothing_result);
+    println!("Lidstone smoothing result: {}", lidstone_smoothing_result);
+    println!("--------------------------------");
+}
+
+fn laplace_smoothing_result(data_parser: &DataParser, test_email_tokens: &Vec<u32>) -> String {
+    let laplace = Laplace::new(data_parser, 1.0, test_email_tokens);
     let prob_ham = laplace.log_prob_ham();
     let prob_spam = laplace.log_prob_spam();
 
-    // If the probability of the email being ham is greater than the probability of the email being spam, then the email is ham
     if prob_ham > prob_spam {
-        println!("The email is ham with a difference of {:.3}.", prob_ham - prob_spam);
+        return format!("The email is ham with a difference of {:.3}.", prob_ham - prob_spam);
     } else {
-        println!("The email is spam with a difference of {:.3}.", prob_spam - prob_ham);
+        return format!("The email is spam with a difference of {:.3}.", prob_spam - prob_ham);
     }
 }
+
+fn lidstone_smoothing_result(data_parser: &DataParser, test_email_tokens: &Vec<u32>) -> String {
+    let lidstone = Lidstone::new(data_parser, 0.5, test_email_tokens);
+    let prob_ham = lidstone.log_prob_ham();
+    let prob_spam = lidstone.log_prob_spam();
+
+    if prob_ham > prob_spam {
+        return format!("The email is ham with a difference of {:.3}.", prob_ham - prob_spam);
+    } else {
+        return format!("The email is spam with a difference of {:.3}.", prob_spam - prob_ham);
+    }
+}
+
